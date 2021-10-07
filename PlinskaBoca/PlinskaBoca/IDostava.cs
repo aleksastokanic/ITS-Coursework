d927a469c1c86715dc4811f00bc69aca35303e67
aleksastokanic <92103962+aleksastokanic@users.noreply.github.com>
2021-10-07 16:16:01 +0200

    Gas program - Plinske boce

---
+++ PlinskaBoca/PlinskaBoca/IDostava.cs
@@ -0,0 +1,22 @@
+﻿
+/*
+ * Klasa Traka implementira intefejs IDostava, sa metodama:
+Utovari (Boca b), Boca Istovari () i propertijem int SlobMesta, koji treba koristiti kao
+indikator pri pokušaju utovara sledeće plinske boce iz kolekcije Boca (utovar boca je moguć
+jedino ako ima slobodnih mesta).
+Ukoliko pri pokušaju utovara nema više slobodnih mesta na traci, ispisati na konzoli
+odgovarajuću poruku, kao i u slučaju da pri pokušaju istovara nema više boca na transportnoj
+traci.
+Pretpostaviti da je kapacitet transportne trake fiksan i poznat, a u cilju testiranja programa
+pretpostaviti da iznosi 5.
+*/
+namespace PlinskaBoca
+{
+    interface IDostava
+    {
+        int SlobMesta { get; set; }
+
+        void utovari(PlinskaBoca b);
+        void istovari();
+    }
+}
---
+++ PlinskaBoca/PlinskaBoca/exNemaMesta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlinskaBoca
+{
+    class exNemaMesta : Exception
+    {
+        public exNemaMesta()
+        {
+            Console.WriteLine("\nIZUZETAK: Nema više mesta za boce!");
+        }
+    }
+}
