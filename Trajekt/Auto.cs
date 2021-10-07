74296f5354d685ce42e592801ef3c9c32abfe36a
aleksastokanic <92103962+aleksastokanic@users.noreply.github.com>
2021-10-07 16:17:12 +0200

    Ferry program - Trajekt

---
+++ Trajekt/Auto.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Trajekt
+{
+    class Auto
+    {
+        string marka;
+        string registracija;
+
+        public Auto(string reg, string marka)
+        {
+            this.marka = marka;
+            registracija = reg;
+        }
+
+        public override string ToString()
+        {
+            return "Reg: " + registracija + ", Marka: " + marka;
+        }
+
+    }
+}
---
+++ Trajekt/ISidrenje.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Trajekt
+{
+    interface ISidrenje
+    {
+        void Utovar(Auto v);
+        Auto Istovar();
+        int SlobodnaMesta { get; }
+    }
+}
