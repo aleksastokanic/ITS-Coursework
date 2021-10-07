5f2a74acec3fd8f344c6eae04c07c9806268a803
aleksastokanic <92103962+aleksastokanic@users.noreply.github.com>
2021-10-07 16:11:11 +0200

    Hrana - Food program

---
+++ Hrana/ConsoleApp1/Hrana.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hrana
+    {
+        double cena;
+        int brojKalorija;
+        string naziv;
+
+        public double Cena { get => cena; set => cena = value; }
+        public int BrojKalorija { get => brojKalorija; set => brojKalorija = value; }
+        public string Naziv { get => naziv; set => naziv = value; }
+
+        public Hrana(double c, int kalorije, string n)
+        {
+            Cena = c;
+            BrojKalorija = kalorije;
+            Naziv = n;
+        }
+
+        public double prodajnaCena()
+        {
+            double ncena = Cena;
+            if (this.BrojKalorija>1000)
+            {
+                ncena *= 1.2;
+            }
+            return ncena;
+        }
+
+        public override string ToString()
+        {
+            return "Naziv: " + naziv;
+        }
+    }
+}
---
+++ Hrana/ConsoleApp1/Korpa.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Korpa : IKupovina, IEnumerable
+    {
+        private List<Hrana> hrana_kupljeno = new List<Hrana>();
+
+
+        // private Hrana[] hrana_kupljeno = new Hrana[50];
+        /*
+        public Hrana this[int indeks]
+        {
+            get
+            {
+                return hrana_kupljeno[indeks];
+            }
+
+            set
+            {
+                hrana_kupljeno[indeks] = value;
+            }
+        }
+        */
+
+        public Hrana this[int indeks]
+        {
+            get { return hrana_kupljeno[indeks]; }
+            set { hrana_kupljeno[indeks] = value;  }
+        }
+
+
+
+        public void dodajHranu(Hrana h)
+        {
+            if (h.BrojKalorija > 1000) throw new NezdravaHrana("Hrana nije zdrava");
+            else hrana_kupljeno.Add(h);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return hrana_kupljeno.GetEnumerator();
+        }
+
+        public double racunajCenuKorpe()
+        {
+            double suma = 0;
+            foreach (Hrana h in hrana_kupljeno)
+                suma += h.prodajnaCena();
+            return suma;
+        }
+
+        public void izlistaj()
+        {
+            foreach (Hrana h in hrana_kupljeno)
+                Console.WriteLine(h.Naziv);
+        }
+    }
+
+    class NezdravaHrana : Exception
+    {
+        public NezdravaHrana(string poruka)
+        {
+            Console.WriteLine(poruka);
+        }
+    }
+}
