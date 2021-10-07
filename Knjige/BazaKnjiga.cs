36ec4cf3cfc242d8981863c8f3d5ec04cb900870
aleksastokanic <92103962+aleksastokanic@users.noreply.github.com>
2021-10-07 16:12:59 +0200

    Books program - Knjige

---
+++ Knjige/BazaKnjiga.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections.Generic;
+
+namespace Knjige
+{
+    // Deklaracija delegata za obradu knjige:
+    public delegate void DelegatObradeKnjige(Knjiga book);
+
+    class BazaKnjiga
+    {
+        int brojKnjiga = 0;
+        decimal cenaKnjiga = 0.0m;
+        // Lista svih k u bazi:
+        List<Knjiga> list = new List<Knjiga>();
+
+        // konstruktor - popunjavanje baze njiga
+        public BazaKnjiga()
+        {
+            dodajKnjigu(new Knjiga("Programski jezik C", "Brian W. Kernighan i Dennis M. Ritchie", 19.95m, true));
+            dodajKnjigu(new Knjiga("Prokleta avlija", "Ivo Andrić", 39.95m, true));
+            dodajKnjigu(new Knjiga("Derviš i smrt", "Meša Selimović", 129.95m, false));
+            dodajKnjigu(new Knjiga("Nauka – Velika ilustrovana enciklopedija ", "Grupa autora", 12.00m, true));
+        }
+
+        public void dodajKnjigu(Knjiga k)
+        {
+            list.Add(k);
+        }
+
+        internal void formiranjeUkupneCene(Knjiga k)
+        {
+            brojKnjiga += 1;
+            cenaKnjiga += k.Cena;
+        }
+
+        internal decimal srednjaCena()
+        {
+            return cenaKnjiga / brojKnjiga;
+        }
+
+        // Poziv delegata za svaku knjigu sa tvrdim povezom da bi je obradio:
+        public void knjigeSaTvrdimPovezom(DelegatObradeKnjige dok)
+        {
+            foreach (Knjiga b in list)
+            {
+                if (b.Tvrdpovez)
+                    // Poziv delegata:
+                    dok(b);
+            }
+        }
+
+        public void sveKnjige(DelegatObradeKnjige dok)
+        {
+            foreach (Knjiga b in list)
+            {
+                // Poziv delegata:
+                dok(b);
+            }
+        }
+    }
+
+}
---
+++ Knjige/Knjiga.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Knjige
+{
+    // Opis k u listi k:
+    public class Knjiga   // Obavezno staviti public da bi delegat mogao biti postavljen u namespace!
+    {
+        public string Naslov;  // Naslov knjige.
+        public string Autor;   // Autor knjige.
+        public decimal Cena;   // Cena knjige.
+        public bool Tvrdpovez; // Da li je tvrd povez?
+        //public static int broj=0;
+
+        public Knjiga(string naslov, string autor, decimal cena, bool tvrdPovez)
+        {
+            Naslov = naslov;
+            Autor = autor;
+            Cena = cena;
+            Tvrdpovez = tvrdPovez;
+        }
+
+        public override string ToString()
+        {
+            return "" + Naslov;
+        }
+    }
+}
